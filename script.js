7466c51938bfc9b31cedbca07323266d309e0100
Timothy McDermott <162518123+timwebdev1@users.noreply.github.com>
2025-06-22 19:54:13 -0500

    created and tested cleanUserInput function

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-console.log("JavaScript is connected!");
-
 let userInput = document.getElementById("text-input").value;
 console.log(userInput);
 
+function cleanUserInput(userInput) {
+  let cleanString = userInput.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  return cleanString;
+}
+
+console.log(cleanUserInput("Bob"));
+console.log(cleanUserInput("A man a plan a canal Panama"));
+console.log(cleanUserInput("race car"));
